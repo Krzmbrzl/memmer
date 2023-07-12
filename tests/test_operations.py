74f31e6e03c74b6b62e5df91f77d853b6d92f20a
Robert Adam <dev@robert-adam.de>
2023-07-12 20:13:57 +0200

    Prepare test case for monthly fee calculation

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -15,12 +15,53 @@ import sqlalchemy.orm
 import sqlalchemy.exc
 from sqlalchemy import select
 
-from memmer.orm import Base, Member, Session, Relation
-from memmer.queries import are_related, make_relation, drop_relation, get_relatives
+from memmer.orm import Base, Member, Session, Relation, FixedCost
+from memmer.queries import are_related, make_relation, drop_relation, get_relatives, get_fixed_cost
+from memmer import AdmissionFeeKey, BasicFeeAdultsKey, BasicFeeYouthsKey, ProcessingFeeKey
 
 
 working_dir = os.path.dirname(os.path.realpath(__file__))
 test_data_dir = os.path.join(working_dir, "test_data")
+
+
+def get_users(session):
+    sally = session.scalars(
+        select(Member).where(
+            (Member.first_name == "Sally") & (Member.last_name == "Smoldriski")
+        )
+    ).first()
+    sam = session.scalars(
+        select(Member).where(
+            (Member.first_name == "Sam") & (Member.last_name == "Smoldriski")
+        )
+    ).first()
+    dirk = session.scalars(
+        select(Member).where(
+            (Member.first_name == "Dirk") & (Member.last_name == "Nowitzki")
+        )
+    ).first()
+
+    assert sally != None
+    assert sam != None
+    assert dirk != None
+
+    return (sally, sam, dirk)
+
+
+def get_sessions(session):
+    shortSession = session.scalars(
+        select(Session).where(Session.name == "Short session")
+    )
+    mediumSession = session.scalars(
+        select(Session).where(Session.name == "Medium session")
+    )
+    longSession = session.scalars(select(Session).where(Session.name == "Long session"))
+
+    assert shortSession != None
+    assert mediumSession != None
+    assert longSession != None
+
+    return (shortSession, mediumSession, longSession)
 
 
 # Taken from https://stackoverflow.com/a/16310732/3907364
@@ -71,55 +112,55 @@ class TestOperations(unittest.TestCase):
             assert sam != None
             session.add(Relation(first_id=sally.id, second_id=sam.id))
 
+            # set fixed costs
+            fixed_costs = [ FixedCost(name=AdmissionFeeKey, costs = 15), FixedCost(name=BasicFeeAdultsKey, cost=15), FixedCost(name=BasicFeeYouthsKey,
+                                                                                                                               cost=4),
+                           FixedCost(name=ProcessingFeeKey, cost=15)]
+            session.add_all(fixed_costs)
+
             session.commit()
 
     def test_relationships(self):
         with self.Session() as session:
-            sally = session.scalars(
-                select(Member).where(
-                    (Member.first_name == "Sally") & (Member.last_name == "Smoldriski")
-                )
-            ).first()
-            sam = session.scalars(
-                select(Member).where(
-                    (Member.first_name == "Sam") & (Member.last_name == "Smoldriski")
-                )
-            ).first()
-            yoshi = session.scalars(
-                select(Member).where(
-                    (Member.first_name == "Yoshi") & (Member.last_name == "Young")
-                )
-            ).first()
-            assert sally != None
-            assert sam != None
-            assert yoshi != None
+            sally, sam, dirk = get_users(session)
 
             # Check current relations
             self.assertTrue(are_related(session, sam, sally))
             self.assertTrue(are_related(session, sally, sam))
-            self.assertFalse(are_related(session, sally, yoshi))
-            self.assertFalse(are_related(session, yoshi, sally))
+            self.assertFalse(are_related(session, sally, dirk))
+            self.assertFalse(are_related(session, dirk, sally))
 
-            # Add yoshi into the relation
-            make_relation(session, sally, yoshi)
-            make_relation(session, yoshi, sam)
-
-            self.assertTrue(are_related(session, sam, sally))
-            self.assertTrue(are_related(session, sally, yoshi))
-            self.assertTrue(are_related(session, yoshi, sam))
-
-            # Remove yoshi again
-            drop_relation(session, sally, yoshi)
-            drop_relation(session, yoshi, sam)
+            # Add dirk into the relation
+            make_relation(session, sally, dirk)
+            make_relation(session, dirk, sam)
 
             self.assertTrue(are_related(session, sam, sally))
-            self.assertFalse(are_related(session, sally, yoshi))
-            self.assertFalse(are_related(session, yoshi, sam))
+            self.assertTrue(are_related(session, sally, dirk))
+            self.assertTrue(are_related(session, dirk, sam))
+
+            # Remove dirk again
+            drop_relation(session, sally, dirk)
+            drop_relation(session, dirk, sam)
+
+            self.assertTrue(are_related(session, sam, sally))
+            self.assertFalse(are_related(session, sally, dirk))
+            self.assertFalse(are_related(session, dirk, sam))
 
             # Get relatives
             self.assertListEqual(get_relatives(session, sally), [sam])
             self.assertListEqual(get_relatives(session, sam), [sally])
-            self.assertListEqual(get_relatives(session, yoshi), [])
+            self.assertListEqual(get_relatives(session, dirk), [])
+
+    def test_monthly_fee_calculation(self):
+        with self.Session() as session:
+            sally, sam, dirk = get_users(session)
+            shortSession, mediumSession, longSession = get_sessions(session)
+
+            self.assertEqual(compute_monthly_fee(sally), get_fixed_cost(BasicFeeYouthsKey))
+            self.assertEqual(compute_monthly_fee(dirk), get_fixed_cost(BasicFeeAdultsKey))
+
+            #sally.participating_sessions.append(shortSession)
+
 
 
 if __name__ == "__main__":
