2347a0078c4014be06e9d49f963529d1cb9a31d4
Robert Adam <dev@robert-adam.de>
2023-08-20 15:04:13 +0200

    Added script to start GUI

---
+++ bin/memmer_gui.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# This file is part of memmer. Use of this source code is
+# governed by a BSD-style license that can be found in the
+# LICENSE file at the root of the source tree or at
+# <https://github.com/Krzmbrzl/memmer/blob/main/LICENSE>.
+
+from memmer.gui import MemmerGUI
+
+def main():
+    gui = MemmerGUI()
+
+    gui.show_and_execute()
+
+
+
+if __name__ == "__main__":
+    main()
