0ab7c1527d28c6b97902d6c037a9cce604935f8b
Robert Adam <dev@robert-adam.de>
2023-08-20 18:01:22 +0200

    create_database: Set up some fixed costs as well

--- bin/create_database.py
+++ bin/create_database.py
@@ -1,14 +1,29 @@
 #!/usr/bin/env python3
 
-from sqlalchemy import create_engine
+from decimal import Decimal
 
-from memmer.orm import Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+from memmer.orm import Base, FixedCost
+from memmer import AdmissionFeeKey, BasicFeeAdultsKey, BasicFeeYouthsKey
 
 
 def main():
     engine = create_engine("sqlite:///sampleDB.sqlite", echo=True)
 
     Base.metadata.create_all(engine)
+
+    with Session(bind=engine) as session:
+        session.add_all(
+            [
+                FixedCost(name=AdmissionFeeKey, cost=Decimal("15")),
+                FixedCost(name=BasicFeeAdultsKey, cost=Decimal("5")),
+                FixedCost(name=BasicFeeYouthsKey, cost=Decimal("4")),
+            ]
+        )
+
+        session.commit()
 
 
 if __name__ == "__main__":
